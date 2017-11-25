e1da63ef50781e9c7655a761b994facbc40b06f8
Damien Le Berrigaud <damien@itsdamo.com>
2017-11-25 15:46:55 -0700

    Disable splash.

--- source/Main.hx
+++ source/Main.hx
@@ -7,6 +7,6 @@ class Main extends Sprite {
 
     public function new() {
         super();
-        addChild(new FlxGame(0, 0, menu.MainMenuState));
+        addChild(new FlxGame(0, 0, menu.MainMenuState, 1, 60, 60, true));
     }
 }
